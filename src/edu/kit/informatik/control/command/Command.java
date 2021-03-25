ba09e73a9afda763f34abf2e34a6db39d12c7ff9
OlloKuster <ufexb@student.kit.edu>
2021-03-25 17:38:27 +0100

    Added the execute method

--- src/edu/kit/informatik/control/command/Command.java
+++ src/edu/kit/informatik/control/command/Command.java
@@ -1,4 +1,11 @@
 package edu.kit.informatik.control.command;
 
+import edu.kit.informatik.model.firebreaker.GameException;
+import edu.kit.informatik.view.Session;
+
+import java.util.List;
+
 public interface Command {
+
+    String execute(Session session, List<String> arguments) throws GameException;
 }

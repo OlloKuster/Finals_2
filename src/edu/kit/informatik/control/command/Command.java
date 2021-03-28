b0370b64e48d760f1dd20c939b05c23a8727e4d2
OlloKuster <ufexb@student.kit.edu>
2021-03-28 21:26:13 +0200

    Before/after win differentiation

--- src/edu/kit/informatik/control/command/Command.java
+++ src/edu/kit/informatik/control/command/Command.java
@@ -4,37 +4,52 @@ import edu.kit.informatik.control.messages.Errors;
 import edu.kit.informatik.model.commands.*;
 import edu.kit.informatik.model.firebreaker.GameException;
 import edu.kit.informatik.view.Session;
+import edu.kit.informatik.view.game.GameState;
 
 import java.util.List;
 
 public interface Command {
 
-    static Command fromString(final String string) throws GameException {
-        switch(string) {
-            case "move":
-                return new MoveCommand();
-            case "extinguish ":
-                return new ExtinguishCommand();
-            case "refill":
-                return new RefillCommand();
-            case "buy-fire-engine":
-                return new BuyFireEngineCommand();
-            case "fire-to-roll":
-                return new FireToRollCommand();
-            case "turn":
-                return new TurnCommand();
-            case "reset":
-                return new ResetCommand();
-            case "show-board":
-                return new ShowBoardCommand();
-            case "show-field":
-                return new ShowFieldCommand();
-            case "show-player":
-                return new ShowPlayerCommand();
-            case "quit":
-                return new QuitCommand();
-            default:
-                throw new GameException(Errors.UNKNOWN_COMMAND);
+    static Command fromString(final String string, GameState gameState) throws GameException {
+        if (gameState.isWin()) {
+            switch (string) {
+                case "show-board":
+                    return new ShowBoardCommand();
+                case "show-field":
+                    return new ShowFieldCommand();
+                case "quit":
+                    return new QuitCommand();
+                default:
+                    throw new GameException(Errors.UNKNOWN_COMMAND);
+            }
+        }
+        else {
+            switch(string) {
+                case "move":
+                    return new MoveCommand();
+                case "extinguish":
+                    return new ExtinguishCommand();
+                case "refill":
+                    return new RefillCommand();
+                case "buy-fire-engine":
+                    return new BuyFireEngineCommand();
+                case "fire-to-roll":
+                    return new FireToRollCommand();
+                case "turn":
+                    return new TurnCommand();
+                case "reset":
+                    return new ResetCommand();
+                case "show-board":
+                    return new ShowBoardCommand();
+                case "show-field":
+                    return new ShowFieldCommand();
+                case "show-player":
+                    return new ShowPlayerCommand();
+                case "quit":
+                    return new QuitCommand();
+                default:
+                    throw new GameException(Errors.UNKNOWN_COMMAND);
+            }
         }
     }
 
